27b6b5b664f3c28505c042ec307f52dfe314013e
harry <shsage91@gmail.com>
2021-03-26 18:25:25 +0900

    [ADD] - fibonacci (recursion & memoization)

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,44 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+
+// Solution #1 - iterative solution
+// function fib(n) {
+//   const series = [0, 1];
+//
+//   let index = series.length;
+//   while (series.length - 1 < n) {
+//     series.push(series[index - 1] + series[index - 2]);
+//     index++;
+//   }
+//
+//   return series[n];
+// }
+
+
+// Solution #2 - recursive solution && memoization
+function fib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+function memoize(fn) {
+  const cache = {};
+
+  return function (...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  }
+}
+
+fib = memoize(fib);
 
 module.exports = fib;
